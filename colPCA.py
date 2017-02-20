24c1f5fda402ffed6d6382408164e4807d8534d0
Jannis <jannis@mac18.cns.ox.ac.uk>
2017-02-20 16:12:03 +0000

    resNetFly

--- colPCA.py
+++ colPCA.py
@@ -1,16 +1,24 @@
 import numpy as np
 import glob
 from scipy import misc
+import os
 
 # Read in Images
 numImgs = 50312
 imgs = np.empty([numImgs,112,112,3])
+filenames = os.listdir("C:/Users/Jannis/Dropbox/GitHub/FacePeeper/Data/")
+
 for ind, filename in enumerate(glob.glob('C:/Users/Jannis/Dropbox/GitHub/FacePeeper/Data/*.jpg')):
 #for ind, filename in enumerate(glob.glob('/Users/jannis/Dropbox/github/FacePeeper/Data/*.jpg')):
-	img = misc.imread(filename) 
-	img2 = img * (1/np.amax(img)) # Normalize all images
-	imgs[ind,:,:,:] = img2
+	imgs[ind,:,:,:] = misc.imread(filename) 
 print('Images stored')
+
+# Normalize all images
+imgs -= np.mean(imgs, axis = 0)
+print('Demeaning done')
+imgs /= np.std(imgs, axis = 0)
+
+print('Normalization done')
 
 def compute_PCA(images):
 	# Reshape Images to make each pixel a single datapoint in the RGB space
@@ -24,5 +32,25 @@ def compute_PCA(images):
 	np.savetxt('eigenvalues.txt',eigenvalues)
 	np.savetxt('eigenvectors.txt', U)
 
+def splitTrainTest(images):
+	''' This function splits the normalized images into training and testing dataset in a 85/15 ratio. '''
+	os.makedirs('TrainData')
+	os.makedirs('TestData')
+	shuffler = np.random.choice([0,1],images.shape[0],p = [0.85,0.15]) # Randomize for each image whether it is train(0) or test(1)
+	
+	for img,ind in enumerate(imgs):
+		if shuffler[ind]:
+			misc.imsave('C:/Users/Jannis/Dropbox/GitHub/FacePeeper/TrainData/'+filenames[ind],img)
+		else:
+			misc.imsave('C:/Users/Jannis/Dropbox/GitHub/FacePeeper/TestData/'+filenames[ind],img)
+
+
+
+
+
+
+
+
 compute_PCA(imgs)
+splitTrainTest(imgs)
 
---
+++ helper.py
@@ -0,0 +1,6 @@
+from scipy import misc 
+import numpy as np
+
+img = misc.imread("C:/Users/Jannis/Dropbox/GitHub/FacePeeper/Data/100_12.jpg")
+
+print(img[10:20,10:20,:])
