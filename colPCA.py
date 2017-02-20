379b56718d3c4823df0607b6e7767b5befbc11d7
Jannis <jannis@mac18.cns.ox.ac.uk>
2017-02-20 20:43:39 +0000

    clea

--- colPCA.py
+++ colPCA.py
@@ -2,23 +2,63 @@ import numpy as np
 import glob
 from scipy import misc
 import os
+import time
 
 # Read in Images
-numImgs = 50312
+numImgs = 5
 imgs = np.empty([numImgs,112,112,3])
 filenames = os.listdir("C:/Users/Jannis/Dropbox/GitHub/FacePeeper/Data/")
 
 for ind, filename in enumerate(glob.glob('C:/Users/Jannis/Dropbox/GitHub/FacePeeper/Data/*.jpg')):
 #for ind, filename in enumerate(glob.glob('/Users/jannis/Dropbox/github/FacePeeper/Data/*.jpg')):
-	imgs[ind,:,:,:] = misc.imread(filename) 
+	if ind < numImgs:
+		imgs[ind,:,:,:] = misc.imread(filename) 
 print('Images stored')
 
-# Normalize all images
-imgs -= np.mean(imgs, axis = 0)
-print('Demeaning done')
-imgs /= np.std(imgs, axis = 0)
 
+
+start = time.time()
+
+# Demeaning
+avgIm = np.empty([112,112,3])
+for r in range(112):
+	for c in range(112):
+		for ch in range(3):
+			avgIm[r,c,ch] = np.mean(imgs[:,r,c,ch])
+imgs -= avgIm
+print(time.time()-start)
+print('Demeaning done')
+start = time.time()
+
+
+# Reduce Variance
+varIm = np.empty([112,112,3])
+for r in range(112):
+	for c in range(112):
+		for ch in range(3):
+			varIm[r,c,ch] = np.std(imgs[:,r,c,ch])
+imgs /= varIm
+
+print(time.time()-start)
 print('Normalization done')
+
+im1 = imgs[1,:,:,:]
+misc.imsave('aaa.jpg',im1)
+im2 = misc.imread('aaa.jpg')
+print(np.amax(im1),np.amin(im1),np.amax(im2),np.amin(im2))
+print(im1[7,7,:],im2[7,7,:])
+print(im2.dtype)
+im3 = im2.astype(np.float64)
+
+im3 *= 4/255
+print(np.amax(im3),np.amin(im3))
+im3 -= 2
+print(np.amax(im3),np.amin(im3))
+
+print(im1[60:62,89,:],im3[60:62,89,:])
+
+
+print((im1==im2).all())
 
 def compute_PCA(images):
 	# Reshape Images to make each pixel a single datapoint in the RGB space
@@ -28,29 +68,40 @@ def compute_PCA(images):
 	cov = np.dot(imgsResh.T,imgsResh) / numImgs
 	U, S, V = np.linalg.svd(cov)
 	eigenvalues = np.sqrt(S)
-
+	print(eigenvalues)
 	np.savetxt('eigenvalues.txt',eigenvalues)
 	np.savetxt('eigenvectors.txt', U)
 
 def splitTrainTest(images):
 	''' This function splits the normalized images into training and testing dataset in a 85/15 ratio. '''
-	os.makedirs('TrainData')
-	os.makedirs('TestData')
+	if not os.path.exists('TrainData'):
+		os.makedirs('TrainData')
+	if not os.path.exists('TestData'):
+		os.makedirs('TestData')
+
 	shuffler = np.random.choice([0,1],images.shape[0],p = [0.85,0.15]) # Randomize for each image whether it is train(0) or test(1)
-	
-	for img,ind in enumerate(imgs):
+	counts = np.unique(shuffler,return_counts = True)
+	trainData = np.empty([counts[1][0],112,112,3])
+	testData = np.empty([counts[1][1],112,112,3])
+	countTrain = 0
+	countTest = 0
+
+	shuffler = shuffler.astype(bool)
+	for ind,img in enumerate(images):
 		if shuffler[ind]:
-			misc.imsave('C:/Users/Jannis/Dropbox/GitHub/FacePeeper/TrainData/'+filenames[ind],img)
+			testData[countTest,:,:,:] = img
+			#misc.imsave('C:/Users/Jannis/Dropbox/GitHub/FacePeeper/TestData/'+filenames[ind],img)
 		else:
-			misc.imsave('C:/Users/Jannis/Dropbox/GitHub/FacePeeper/TestData/'+filenames[ind],img)
+			trainData[countTrain,:,:,:] = img
+			#misc.imsave('C:/Users/Jannis/Dropbox/GitHub/FacePeeper/TrainData/'+filenames[ind],img)
+
+	np.savetxt('TrainData.txt',trainData)
+	np.savetxt('TestData.txt',testData)
 
 
 
 
 
-
-
-
-compute_PCA(imgs)
-splitTrainTest(imgs)
+#compute_PCA(imgs)
+#splitTrainTest(imgs)
 
--- helper.py
+++ helper.py
@@ -1,6 +1,18 @@
-from scipy import misc 
 import numpy as np
+import glob
+from scipy import misc
+import os
+import time
 
-img = misc.imread("C:/Users/Jannis/Dropbox/GitHub/FacePeeper/Data/100_12.jpg")
+# Read in Images
+numImgs = 50312
+imgs = np.empty([numImgs,112,112,3])
+filenames = os.listdir("C:/Users/Jannis/Dropbox/GitHub/FacePeeper/Data/")
 
-print(img[10:20,10:20,:])
+for ind, filename in enumerate(glob.glob('C:/Users/Jannis/Dropbox/GitHub/FacePeeper/Data/*.jpg')):
+#for ind, filename in enumerate(glob.glob('/Users/jannis/Dropbox/github/FacePeeper/Data/*.jpg')):
+	if ind < numImgs:
+		imgs[ind,:,:,:] = misc.imread(filename) 
+print('Images stored')
+
+np.savetxt('data.txt',imgs)
