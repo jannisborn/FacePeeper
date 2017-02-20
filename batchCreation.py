a7746c4a0ea5363c992eea2d37e5c192d3cd8cd6
Jannis <jannis@mac18.cns.ox.ac.uk>
2017-02-20 11:24:04 +0000

    edit

--- batchCreation.py
+++ batchCreation.py
@@ -2,25 +2,25 @@
 
 
 # Add this after the tf.placeholder:
-filenames = tf.train.match_filenames_once("./images/*.jpg")
-numImgs = len(filename_queue)
+self.filenames = tf.train.match_filenames_once("./images/*.jpg")
+self.numImgs = len(filename_queue)
 
 
 
 # Call this function when you want to get a batch
 def createBatch(batchSize):
 	''' This function receives a batchSize and randomly loads some images from the dataset.
-	Images are returned as np.array '''
+	Images and labels are returned as np.array '''
 
 	from scipy import misc
 
 	# Variant to read via scipy to np array (maybe slower than TF build in)
-	indices = np.random.randint(0,numImgs,batchSize)
+	indices = np.random.randint(0,self.numImgs,batchSize)
 	images = np.empty([batchSize,112,112,3])
 	labels = np.array(batchSize,dtype=int)
 	for counter,imgInd in enumerate(indices):
-		images[counter,:,:,:] = misc.imread(filenames[imgInd])
-		labels[counter] = int(filenames[imgInd][:3])
+		images[counter,:,:,:] = misc.imread(self.filenames[imgInd])
+		labels[counter] = int(self.filenames[imgInd][:3])
 	return images,labels
 
 	
